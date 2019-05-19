a4c415212a6423e9309b3ac77bbff264184813dd
Aditya Raj <aditya.x510@gmail.com>
2019-05-19 10:16:52 +0530

    init

--- src/admin_area/includes/header.php
+++ src/admin_area/includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Code-Sticky Admin Area</title>
-    <link rel="stylesheet" href="css/bootstrap-337.min.css">
-    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap-337.min.css">
+    <link rel="stylesheet" href="../font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
